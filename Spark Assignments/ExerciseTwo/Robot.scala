de4e51c737d23de8443c23da231de2524d70eb8f
Harikrishnan Naveen <115079327+krishnhk@users.noreply.github.com>
2022-11-03 21:41:53 +0530

    Update Robot.scala

--- Spark Assignments/ExerciseTwo/Robot.scala
+++ Spark Assignments/ExerciseTwo/Robot.scala
@@ -1,5 +1,5 @@
-class Robot(robot_ID: String, robotType: String, mobility : String, universalUID: String) extends Serializable{
- override def toString: String = {
- s"$robot_ID , $robotType , $mobility , $universalUID"
+class Robot(robot_ID: String, robotType: String, mobility: String, universalUID: String) extends Serializable {
+  override def toString: String = {
+    s"$robot_ID , $robotType , $mobility , $universalUID"
   }
 }
