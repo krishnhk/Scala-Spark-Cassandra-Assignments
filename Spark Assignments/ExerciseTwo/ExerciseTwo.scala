20996b06a5472ff11142a25d61b05014545080f9
Harikrishnan Naveen <115079327+krishnhk@users.noreply.github.com>
2022-11-03 21:40:10 +0530

    Update ExerciseTwo.scala

--- Spark Assignments/ExerciseTwo/ExerciseTwo.scala
+++ Spark Assignments/ExerciseTwo/ExerciseTwo.scala
@@ -6,6 +6,19 @@ import scala.util.Random.nextInt
 object ExerciseTwo extends Serializable {
   @transient lazy val logger: Logger = Logger.getLogger(getClass.getName)
 
+  private def generateRobotType: String = List("AMR", "AGV", "Humanoid", "Hybrid", "Articulated Robot", "Cobot")(nextInt(6))
+
+  private def generateRobotUUID: String = UUID.randomUUID.toString
+
+  private def generateRobotID = s"CR-${(0 until 2).map(_ => nextInt(10)).mkString}"
+
+  private def checkMobility(model: String): String = {
+    model match {
+      case "AMR" | "AGV" | "Humanoid" | "Hybrid" => "Mobile"
+      case "Articulated Robot" | "Cobot" => "Immobile"
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder()
       .appName("Exercise Two")
@@ -15,46 +28,36 @@ object ExerciseTwo extends Serializable {
     import spark.implicits._
     logger.info("Spark Session Begins")
 
-    def generateRobotType: String = List("AMR", "AGV", "Humanoid", "Hybrid", "Articulated Robot", "Cobot")(nextInt(6))
+    def runExercise(): Unit = {
 
-    def generateRobotUUID: String = UUID.randomUUID.toString
+      // * Generating details of 10 robots
+      val robot_IDs = (0 until 10).map(_ => (generateRobotID, generateRobotType, generateRobotUUID))
 
-    def generateRobotID = s"CR-${(0 until 2).map(_ => nextInt(10)).mkString}"
+      // * Converting IndexedSeq to RDD
+      val robotRDD = spark.sparkContext.parallelize(robot_IDs).cache()
 
-    def checkMobility(model: String): String = {
-      model match {
-        case "AMR" | "AGV" | "Humanoid" | "Hybrid" => "Mobile"
-        case "Articulated Robot" | "Cobot" => "Immobile"
-      }
+      // * Creating RDD of case class
+
+      val robotArmyRDD = robotRDD.map(row => RobotRecord(row._1, row._2, checkMobility(row._2), row._3))
+      logger.info(robotArmyRDD.collect.mkString(" \n"))
+
+      // * Creating RDD of class
+      val normalRobotRDD = robotRDD.map(row => new Robot(row._1, row._2, checkMobility(row._2), row._3))
+      logger.info(normalRobotRDD.collect.mkString(" \n"))
+
+      // * Creating a DataFrame
+      val robotDF = robot_IDs.toDF("Robot ID", "Type", "Robot UUID")
+      robotDF.show(false)
+
+      // * Creating a DataSet
+      val robotDS = robot_IDs.map(row => RobotRecord(row._1, row._2, checkMobility(row._2), row._3)).toDS()
+      robotDS.show(false)
+
+      robotRDD.unpersist()
     }
-    // * Generating details of 10 robots
-    val robot_IDs = (0 until 10).map(_ => (generateRobotID, generateRobotType, generateRobotUUID))
+    runExercise()
 
-    logger.info("\n\n==> Displaying an RDD of Case Class\n")
-    val robotRDD = spark.sparkContext.parallelize(robot_IDs)
-    val newRobotRDD = robotRDD.map(row => (row._1, row._2, checkMobility(row._2), row._3))
-    val robotArmyRDD = newRobotRDD.map(row => RobotRecord(row._1, row._2, row._3, row._4))
-    logger.info(robotArmyRDD.collect.mkString(" \n"))
-
-
-    logger.info("\n\n==> Displaying an RDD of class\n")
-    val robotRDD2 = spark.sparkContext.parallelize(robot_IDs)
-    val newRobotRDD2 = robotRDD2.map(row => (row._1, row._2, row._3))
-
-    // * Creating RDD of Robot class
-    val robotArmyRDD2 = newRobotRDD2.map(row => new Robot(row._1, row._2, checkMobility(row._2), row._3))
-    logger.info(robotArmyRDD2.collect.mkString(" \n"))
-
-    logger.info("\n\n==> Displaying Data Frame\n")
-    val robotDF = spark.createDataFrame(robot_IDs).toDF("Robot ID", "Type", "Robot UUID")
-    robotDF.show(false)
-
-
-    logger.info("\n\n==> Displaying Data Set")
-    val robotDS = robot_IDs.map(row => RobotRecord(row._1, row._2, checkMobility(row._2), row._3)).toDS()
-    robotDS.show(false)
-
+    logger.info("Spark Session ends")
     spark.stop()
   }
-
 }
