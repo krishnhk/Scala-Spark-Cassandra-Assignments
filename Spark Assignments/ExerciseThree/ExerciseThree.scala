2121b06fc716c7de01d0072745452c3dc34e9e2b
Harikrishnan Naveen <115079327+krishnhk@users.noreply.github.com>
2022-11-03 21:48:31 +0530

    Update ExerciseThree.scala

--- Spark Assignments/ExerciseThree/ExerciseThree.scala
+++ Spark Assignments/ExerciseThree/ExerciseThree.scala
@@ -1,60 +1,57 @@
 import org.apache.log4j.Logger
 import org.apache.spark.sql.SparkSession
-
 import java.util.UUID
 import scala.util.Random.nextInt
 
 object ExerciseThree extends Serializable {
   @transient lazy val logger: Logger = Logger.getLogger(getClass.getName)
 
+  private def getRobotType: String = List("AMR", "AGV", "Humanoid", "Hybrid", "Articulated Robot", "Cobot")(nextInt(6))
+
+  private def generateRobotUUID: String = UUID.randomUUID.toString
+
+  private def generateRobotID = s"CRC-${(0 until 2).map(_ => nextInt(10)).mkString}"
+
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder()
       .appName("Exercise Two")
       .master("local[*]")
       .getOrCreate()
-    logger.info("Spark Session Begins")
-
-
-    def getRobotType: String = List("AMR", "AGV", "Humanoid", "Hybrid", "Articulated Robot", "Cobot")(nextInt(6))
-
-    def generateRobotUUID = UUID.randomUUID.toString
-
-    def generateRobotID = s"RB-${(0 until 2).map(_ => nextInt(10)).mkString}"
 
     import spark.implicits._
+    logger.info("Spark Session Begins")
 
-    // * Creating record of 10 rows
-    val robot_IDs = (0 until 10).map(_ => (generateRobotID, getRobotType, generateRobotUUID))
+    def runExercise(): Unit = {
 
-    logger.info("\n\n==> Data to DataFrame\n")
-    val robotDF = robot_IDs.toDF("RobotID", "Type", "UUID")
-    robotDF.show(false)
+      // * Creating record of 10 rows
+      val robot_IDs = (0 until 10).map(_ => (generateRobotID, getRobotType, generateRobotUUID))
 
+      // * Data to DataFrame
+      val robotDF = robot_IDs.toDF("RobotID", "Type", "UUID")
+      robotDF.show(false)
 
+      // * DataFrame to DataSet
+      val robotDS = robotDF.as[RobotRecord]
+      robotDS.show(false)
 
-    logger.info("\n\n==> DataFrame to DataSet\n")
-    val robotDS = robotDF.as[RobotRecord]
-    robotDS.show(false)
+      // * DataSet to RDD
+      val robotRDD = robotDS.rdd.cache()
+      logger.info(robotRDD.collect().mkString(" \n"))
 
+      // * RDD to DataSet
+      val rddToDs = spark.createDataset(robotRDD)
+      rddToDs.show(false)
 
+      // * RDD to DataFrame
+      val rddToDf = robotRDD.toDF
+      rddToDf.show(false)
 
-    logger.info("\n\n==> DataSet to RDD\n")
-    val robotRDD = robotDS.rdd
-    logger.info(robotRDD.collect().mkString(" \n"))
+      robotRDD.unpersist()
+    }
 
+    runExercise()
 
-
-    logger.info("\n\n==> RDD to DataSet\n")
-    val rddToDs  = spark.createDataset(robotRDD)
-    rddToDs.show(false)
-
-
-
-    logger.info("\n\n==> RDD to DataFrame\n")
-    val rddToDf = robotRDD.toDF
-    rddToDf.show(false)
-
+    logger.info("Spark session ends")
+    spark.stop()
   }
-
-
 }
