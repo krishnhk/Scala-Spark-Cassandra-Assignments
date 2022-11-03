da0910c2f90401d02e67fabf4109ccf0f6120568
Harikrishnan Naveen <115079327+krishnhk@users.noreply.github.com>
2022-11-03 22:06:34 +0530

    Update ExerciseFour.scala

--- Spark Assignments/ExerciseFour/ExerciseFour.scala
+++ Spark Assignments/ExerciseFour/ExerciseFour.scala
@@ -1,11 +1,10 @@
 import org.apache.log4j.Logger
 import org.apache.spark.sql.SparkSession
 
-
-object ExerciseFour extends Serializable{
+object ExerciseFour extends Serializable {
   @transient lazy val logger: Logger = Logger.getLogger(getClass.getName)
-  def main(args:Array[String]):Unit ={
-
+  
+  def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder()
       .appName("Exercise Two")
       .master("local[*]")
@@ -15,96 +14,99 @@ object ExerciseFour extends Serializable{
     import spark.implicits._
 
     val cities = List(
-      ("Tamil Nadu","India"),
-      ("Tokyo","Japan"),
+      ("Tamil Nadu", "India"),
+      ("Tokyo", "Japan"),
       ("London", "England"),
-      ("Amsterdam","Netherlands"),
-      ("Paris","France")
-    )
+      ("Amsterdam", "Netherlands"),
+      ("Paris", "France"))
 
     val continents = List(
-      ("Japan","Asia","Non-Polar"),
-      ("England","Europe","Non-Polar"),
-      ("France","Europe","Non-Polar"),
-      ("India","Asia","Non-Polar"),
-      ("Green Land","Arctic","Polar")
-    )
+      ("Japan", "Asia", "Non-Polar"),
+      ("England", "Europe", "Non-Polar"),
+      ("France", "Europe", "Non-Polar"),
+      ("India", "Asia", "Non-Polar"),
+      ("Green Land", "Arctic", "Polar"))
 
-    logger.info("\n\n==> DataFrame Inner Join\n")
+    def joinRDD(): Unit = {
 
+      val cityBaseRDD = spark.sparkContext.parallelize(cities)
 
+      // * Created a key-tuple pair with primary key - country
+      val cityRDD = cityBaseRDD.keyBy(x => x._2)
 
-    val cityDF = cities.toDF("City","Country")
-    val continentDF = continents.toDF("Country","Continent","Region")
-    val dfJoinExpr = cityDF.col("Country")===continentDF.col("Country")
-    val dfJoinType = "inner"
+      val continentBaseRDD = spark.sparkContext.parallelize(continents)
 
-    cityDF.join(continentDF,dfJoinExpr,dfJoinType)
-      .drop(cityDF.col("Country"))
-      .show()
+      // * Created a key-tuple pair with primary key - country
+      val continentRDD = continentBaseRDD.keyBy(x => x._1)
 
-    val cityList = cities.map(row => City(row._1,row._2))
-    val cityDS = spark.createDataset(cityList)
+      // *  RDD inner join
+      val innerJoinedRDD = cityRDD.join(continentRDD)
+      innerJoinedRDD.foreach(println)
 
+      // * RDD Left outer join
+      val leftOuterRDD = cityRDD.leftOuterJoin(continentRDD)
+      leftOuterRDD.foreach(println)
+    }
 
-    val continentList = continents.map(row => Continent(row._1,row._2,row._3))
-    val continentDS = spark.createDataset(continentList)
+    joinRDD()
+    
+    def joinDataFrame(): Unit = {
 
+      val cityDF = cities.toDF("City", "Country")
+      val continentDF = continents.toDF("Country", "Continent", "Region")
+      val dfJoinExpr = cityDF.col("Country") === continentDF.col("Country")
+      val dfJoinType = "inner"
 
-    val dsJoinType1 = "right"
-    val dsJoinType2 = "inner"
-    val dsJoinExpr = cityDS("country")===continentDS("country")
+      cityDF.join(continentDF, dfJoinExpr, dfJoinType)
+        .drop(cityDF.col("Country"))
+        .show()
+    }
 
-    logger.info("\n\n Dataset Right Outer join\n")
+    joinDataFrame()
 
-    cityDS.joinWith(continentDS,dsJoinExpr,dsJoinType1)
-      .map{
-        // * Null produced from the cityDs in the join operation will be marked as Unknown City
-        case(null,y) => CityContinent("Unknown City",y.continent,y.region)
-        // * Null produced from the continentDs in the join operation will bw marked as Unknown Continent & Unknown region
-       // * To avoid null pointer exception
-        case(x,null) => CityContinent(x.city,"Unknown Continent","Unknown Region")
-        case (x,y) => CityContinent(x.city,y.continent,y.region)
-      }
-      .withColumnRenamed("city","City")
-      .withColumnRenamed("continent","Continent")
-      .withColumnRenamed("region","Region")
-      .show()
+    def joinDataSet(): Unit = {
+      
+      val cityList = cities.map(row => City(row._1, row._2))
+      val cityDS = spark.createDataset(cityList)
 
+      val continentList = continents.map(row => Continent(row._1, row._2, row._3))
+      val continentDS = spark.createDataset(continentList)
 
-    logger.info("\n\n Dataset Inner join\n")
-    cityDS.joinWith(continentDS, dsJoinExpr, dsJoinType2)
-      .map {
-        case (null, y) => CityContinent("Unknown City", y.continent, y.region)
-        case (x, null) => CityContinent(x.city, "Unknown Continent", "Unknown Region")
-        case (x, y) => CityContinent(x.city, y.continent, y.region)
-      }
-      .withColumnRenamed("city", "City")
-      .withColumnRenamed("continent", "Continent")
-      .withColumnRenamed("region", "Region")
-      .show()
+      val dsRightJoinType = "right"
+      val dsInnerJoinType = "inner"
+      val dsJoinExpr = cityDS("country") === continentDS("country")
 
-    val cityBaseRDD = spark.sparkContext.parallelize(cities)
+      // * Dataset Right join
+      cityDS.joinWith(continentDS, dsJoinExpr, dsRightJoinType)
+        .map {
+          // * Null produced from the cityDs in the join operation will be marked as Unknown City
+          case (null, y) => CityContinent("Unknown City", y.continent, y.region)
+          // * Null produced from the continentDs in the join operation will bw marked as Unknown Continent & Unknown region
+          // * To avoid null pointer exception
+          case (x, null) => CityContinent(x.city, "Unknown Continent", "Unknown Region")
+          case (x, y) => CityContinent(x.city, y.continent, y.region)
+        }
+        .withColumnRenamed("city", "City")
+        .withColumnRenamed("continent", "Continent")
+        .withColumnRenamed("region", "Region")
+        .show()
+      
+      // * Dataset inner join
+      cityDS.joinWith(continentDS, dsJoinExpr, dsInnerJoinType)
+        .map {
+          case (null, y) => CityContinent("Unknown City", y.continent, y.region)
+          case (x, null) => CityContinent(x.city, "Unknown Continent", "Unknown Region")
+          case (x, y) => CityContinent(x.city, y.continent, y.region)
+        }
+        .withColumnRenamed("city", "City")
+        .withColumnRenamed("continent", "Continent")
+        .withColumnRenamed("region", "Region")
+        .show()
+    }
 
-    // * Created a key-tuple pair with primary key - country
-    val cityRDD = cityBaseRDD.keyBy(x=> x._2)
+    joinDataSet()
 
-
-    val continentBaseRDD = spark.sparkContext.parallelize(continents)
-
-    // * Created a key-tuple pair with primary key - country
-    val continentRDD = continentBaseRDD.keyBy(x=>x._1)
-
-    logger.info("\n\nRDD Inner Join\n")
-
-    val innerJoinedRDD = cityRDD.join(continentRDD)
-    innerJoinedRDD.foreach(println)
-
-    logger.info("\n\nRDD Left Outer Join\n")
-
-    val leftOuterRDD = cityRDD.leftOuterJoin(continentRDD)
-    leftOuterRDD.foreach(println)
-
+    logger.info("Spark session ends")
+    spark.stop()
   }
-
 }
