6830bff7998f1b6946c78faa008d3afcd09c1377
Harikrishnan Naveen <115079327+krishnhk@users.noreply.github.com>
2022-11-03 21:36:52 +0530

    Update Header.scala

--- Spark Assignments/ExerciseOne/Header.scala
+++ Spark Assignments/ExerciseOne/Header.scala
@@ -1,3 +1,3 @@
 import java.time.LocalDate
 
-case class Header(camp_ID: Int, individual_ID: Long, passCode: String, date_Of_Birth: LocalDate,cutOff:Int,major:Boolean)
+case class Header(camp_ID: Int, individual_ID: Long, passCode: String, date_Of_Birth: LocalDate, cutOff: Int, major: String)
